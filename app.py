0b0e206c69a131d9e9a18c3f5123d691156dd0d2
James Moultrie <82656270+mayze13@users.noreply.github.com>
2023-03-13 12:03:50 +0100

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,21 @@ import streamlit as st
 from bs4 import BeautifulSoup
 from scipy.io import wavfile
 
+st.markdown(f"<h1 style='text-align: center;'> 🤖 Music Generator 🤖 </h1>", unsafe_allow_html=True)
+style = st.sidebar.selectbox(
+        "🎹 Select your style 🎸",
+        ("Daft Punk", "J.S. Bach", "Britney Spears"))
+temperature = st.sidebar.slider('🌶️ Spice levels 🌶️', 0.1, 1.0, 0.01)
+bars = st.sidebar.select_slider('How many bars?', options=[4, 8, 16])
+
+url = ''
+params = {
+"style": style,
+"temperature":temperature,
+"bars":bars}
 
 
-st.markdown(f"<h1 style='text-align: center;'> NOTHING HERE YET </h1>", unsafe_allow_html=True)
+response = requests.get(url, params)
+# fare = round(response.json()['fare'])
+
+st.markdown(f'# Nothing to show here yet :)')
